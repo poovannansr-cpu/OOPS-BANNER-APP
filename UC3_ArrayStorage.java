aad6373e49b4e8cd8b7331099071073b43ba7e7c
poovannansr-cpu <poovannansr@gmail.com>
2026-03-03 10:25:34 +0530

    Add files via upload

---
+++ UC3_ArrayStorage.java
@@ -0,0 +1,15 @@
+public class UC3_ArrayStorage {
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "OOPS",
+                "OOPS",
+                "OOPS",
+                "OOPS"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
